2863ca4241b1aa0a0e03ac2467b78f04ef6b13ed
Soohimi <soohimi2003@gmail.com>
2025-10-05 12:43:56 +0330

    add daisyui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  return (
+    <div>
+      <h1>Users</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UsersPage;
