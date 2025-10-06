4721afd815f151371892fdf93bf0243c64e72a5f
Soohimi <soohimi2003@gmail.com>
2025-10-06 17:48:13 +0330

    add loading

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
 }
-
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <div>
       <h1>Users</h1>
-      <UserTable sortOrder={sortOrder} />
+      <Link href={"/users/new"} className="btn">
+        NEW USER
+      </Link>
+      <Suspense
+        fallback={<span className="loading loading-infinity loading-lg"></span>}
+      >
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </div>
   );
 };
